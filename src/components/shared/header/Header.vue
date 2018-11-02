21978d1f648c97cfb0e3faba08da39aa7117f724
Fernando Silva Santos <fernandosilva.smsrio@gmail.com>
2018-11-02 21:19:03 -0200

    Estrutura do Header criada

--- src/components/shared/header/Header.vue
+++ src/components/shared/header/Header.vue
@@ -1,6 +1,14 @@
 <template>
-    <header class="app-header">
-        aa
+    <header class="app-header bordered">
+        <div class="app-header__logo bordered">
+            <h1>Logo</h1>
+        </div>
+        <div class="app-header__title bordered">
+            <h2>Listagem de Produtos</h2>
+        </div>
+        <div class="app-header__user bordered">
+            <span>Seja bem vindo, Fernando!</span>
+        </div>
     </header>
 </template>
 
@@ -12,8 +20,33 @@
 
 <style lang="scss">
     .app-header{
-        height: 30px;
-        border: 1px solid black;
+        display: flex;
+        padding: 3px;
+        font-size: 9px;
+        &__logo h1{
+            margin: 10px;
+        }
+        &__title {
+            display: flex;
+            justify-content: flex-end;
+            flex: 1 1 auto;
+        }        
+        &__user {
+            display: none;
+        }    
+    }
+    @media (min-width: 900px) {
+        .app-header{
+            font-size: initial;
+            &__title {
+                justify-content: center;
+            }        
+            &__user {
+                display: block;
+                align-self: center;
+                font-size: 12px;
+            }
+        }        
     }
 </style>
 
